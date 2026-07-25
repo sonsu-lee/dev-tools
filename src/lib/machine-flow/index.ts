45347b1292eded4f5157a658d65b2436ca9ab7d7
sonsu-lee <leesonsu07@gmail.com>
2026-07-26 02:04:34 +0900

    refactor: remove unused state scaffolding

--- src/lib/machine-flow/index.ts
+++
@@ -1,6 +0,0 @@
-export { defineMachineFlow, type MachineFlow } from './define-machine-flow';
-export {
-  useMachineFlow,
-  type MachineFlowActorOptions,
-  type MachineFlowResult,
-} from './use-machine-flow';
